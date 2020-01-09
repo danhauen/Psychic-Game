7c89b2d227ca9e3ccbb0ebc2e42c339d54672c1c
Dan Hauenstein <danhauen@gmail.com>
2020-01-09 00:10:24 -0600

    more functionality. still not perfect. glitch with reset button and your guess working properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,19 +5,24 @@
   // computer choses random letter
       var randLetter = letters [Math.floor(Math.random() * letters.length)];
   
-  // starts wins and losses at zero and initial guess amount at 5
+  // starts wins and losses at zero and initial guess amount at 9
       var winCount = 0;
       var lossCount = 0;
-      var guessesLeft= 5;
-      
-      
+      var guessesLeft= 9;
+      var guessedLetters = [];
 
+  // var for joining guessed letters
+      var updateGuessesSoFar = function() {
+        document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");   
+      };
 
   // vars for diplaying text HTML
       var userGuessText = $("#userGuess-display");
       var winsText = $("#win-display")
       var lossesText = $("#loss-display");
       var guessesLeftText = $("#guessesLeft-display");
+      
+      
       
       winsText.text(winCount);
       lossesText.text(lossCount);
@@ -28,26 +33,22 @@
       $("#button").on("click", function() {
         randLetter = letters [Math.floor(Math.random() * letters.length)];    
         winCount = 0;
-        winsText.text(winCount);
+        //winsText.text(winCount);
         lossCount = 0;
-        lossesText.text(lossCount);
-        guessesLeft= 5;
-        guessesLeftText.text(guessesLeft);
-        userGuess = [];
-        userGuessText.text(userGuess); 
+        //lossesText.text(lossCount);
+        guessesLeft= 9;
+        //guessesLeftText.text(guessesLeft);
+        //guessedLetters = [];  
         console.log(randLetter);
       });
 
    // reset when letter guessed or exceeds guess limit of 5
       function reset() {
         randLetter = letters [Math.floor(Math.random() * letters.length)]; 
-        console.log(randLetter);
         winsText.text(winCount);
         lossesText.text(lossCount);
-        guessesLeft = 5;
-        guessesLeftText.text(guessesLeft);
-        userGuess = [];
-        userGuessText.text(userGuess);     
+        guessesLeft = 9;
+        console.log(randLetter);    
       }   
       
       
@@ -57,38 +58,42 @@ $(document).ready(function() {
             
 
     // user presses a key to guess letter
-    document.onkeyup = function(event) {
-      var userGuess = event.key.toLowerCase();
-      userGuessText.text(userGuess);
-
-      // changes wins and losses
+      document.onkeyup = function(event) {
+        var userGuess = event.key.toLowerCase();
+    //userGuessText.text(userGuess);
+      guessedLetters.push(userGuess);
+     
+     
+    // changes wins and losses
       winsText.text(winCount);
       lossesText.text(lossCount);
-      // updates amount of guesses remaining
+    // updates amount of guesses remaining
       guessesLeftText.text(guessesLeft);
-      //guessesSoFarText.text(userGuess);
+      
 
 
                       if (userGuess === randLetter) {
-                        winCount++;
-                        console.log("correct")
-                        reset();
-                        
+                          winCount++;
+                          reset();
+
+                          console.log("correct")
                       }   
 
                       else if (userGuess != randLetter) {
-                        guessesLeft--;
-                        console.log("wrong")
-                        guessesLeftText.text(guessesLeft);
-                        //guessedLettersText.text(guessedLetters);
-
-                                    if (guessesLeft === 0){
-                                    lossCount++;
-                                    guessesLeft = 5;
-                                    console.log("loss")
-                                    reset();
-                                    }  
-                        }     
+                          guessesLeft--;
+                          guessesLeftText.text(guessesLeft);  
+                          updateGuessesSoFar(); 
+                          console.log("wrong")
+                      }    
+                                    
+                      if (guessesLeft === 0){
+                          lossCount++;
+                          guessedLetters = [];
+                          updateGuessesSoFar(); 
+                          reset();
+                          console.log("loss")
+                      }  
+                             
                     } 
                   });        
 
